59fa999044ff06be0f1e0876635955582a89f70f
Andrey680-star <sa5981225@gmail.com>
2025-10-07 20:20:15 +0300

    Update script.js

--- script.js
+++ script.js
@@ -12,4 +12,18 @@ document.getElementById('formular').addEventListener('submit', function(e){
     e.preventDefault();
     document.getElementById('mesaj').textContent = `Formular pentru cont ${cont}€ trimis cu succes!`;
 });
+// Preia contul din URL
+const params = new URLSearchParams(window.location.search);
+const cont = params.get('cont');
+
+if (cont) {
+    document.getElementById('titlu-cont').textContent = `Formular Cont ${cont}€`;
+    document.getElementById('cont-selectat').value = cont;
+}
+
+// Simulare submit
+document.getElementById('formular').addEventListener('submit', function(e){
+    e.preventDefault();
+    document.getElementById('mesaj').textContent = `Formular pentru cont ${cont}€ trimis cu succes!`;
+});
 
